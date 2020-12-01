99962209a848a581363c2296ad45ce2b17678a8f
Kevin Kivi <nake89@gmail.com>
2020-12-01 18:55:51 +0200

    feat: Doggo feature

--- packages/core/index.js
+++ packages/core/index.js
@@ -2,3 +2,11 @@ console.log("Hello World")
 console.log("By nake89")
 console.log("More stuff")
 console.log("This is a feature")
+
+let doggo = "good"
+
+if (doggo == "good") {
+  console.log("Doggo is good")
+} else {
+  console.log("Bad doggo")
+}
